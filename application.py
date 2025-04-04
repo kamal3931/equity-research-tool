9785101c87bd62c7b4935c15c26f9590c5ec8222
kamal3931 <kamalparekh515@gmail.com>
2025-04-04 11:40:34 -0400

    Update application.py

--- application.py
+++ application.py
@@ -1,7 +1,6 @@
 import pickle
 import os
 import streamlit as st
-from langchain.vectorstores import FAISS
 from dotenv import load_dotenv
 from langchain import OpenAI
 from langchain_openai import OpenAI
@@ -14,7 +13,6 @@ from langchain.chains import RetrievalQAWithSourcesChain
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.document_loaders import UnstructuredURLLoader
 from langchain.embeddings import OpenAIEmbeddings
-from langchain.vectorstores import FAISS
 st.title("Equity reseach tool")
 try:
     api_key = st.secrets["OPENAI_API_KEY"]
